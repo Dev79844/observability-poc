61db80aecab8d1654e932092341b30fe9af0e125
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2024-10-08 22:27:54 +0530

    added: custom metric

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var totalRequests = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_reuqest_total",
+		Help: "Number of get requests",
+	},
+	[]string{"path"},
+)
+
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+
+		totalRequests.WithLabelValues("path").Inc()
+	})
+}
+
+func init(){
+	prometheus.Register(totalRequests)
+}
 
 func main(){
 	router := mux.NewRouter()
 
+	router.Path("/metrics").Handler(promhttp.Handler())
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
 	log.Println("server started on port 3000")
 	err := http.ListenAndServe(":3000", router)
 	log.Fatal(err)
